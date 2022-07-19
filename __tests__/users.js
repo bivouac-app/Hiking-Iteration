5262035992f84ace974d469bdd5abb0183922958
CE Martin <16669684+candacemartin@users.noreply.github.com>
2022-07-19 19:56:47 -0400

    user schema testing

--- __tests__/users.js
+++ __tests__/users.js
@@ -4,11 +4,46 @@ const fs = require('fs');
 const path = require('path');
 const User = require('../server/models/usersModel');
 const { dbConnect, dbDisconnect } = require('./utils/dbHandler.utils.js');
+const {
+  validateNotEmpty,
+  validateStringEquality,
+  validateMongoDuplicationError,
+} = require('./utils/validators.utils');
 
 const fakeUser = {
-  firstName : { type : String, required : true } ,
-  lastName : , 
+  firstName : 'Test',
+  lastName : 'Dummy', 
   location : 'New York',
   email : 'abc@gmail.com',
   password : 'abc123',
 };
+
+beforeAll(async () => dbConnect());
+afterAll(async () => dbDisconnect());
+
+describe('User Model Test Suite',  () => {
+  test('Should add a user to the database', async () => {
+    const validUser =  new User(fakeUser);
+    const savedUser = await validUser.save();
+
+    console.log('savedUser',savedUser);
+    console.log('validUser', validUser)
+    expect(savedUser.firstName).toNotBeNull();
+  });
+
+  test('should validate MongoError duplicate error with code 11000', async () => {
+    expect.assertions(4);
+    const validStudentUser = new User({
+      local: fakeUserData,
+      role: fakeUserData.role,
+    });
+    try {
+      await validStudentUser.save();
+    } catch (error) {
+      const { name, code } = error;
+      validateMongoDuplicationError(name, code);
+    }
+  });
+})
+
+
--- __tests__/utils/dbHandler.utils.js
+++ __tests__/utils/dbHandler.utils.js
@@ -3,7 +3,9 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoServer = new MongoMemoryServer();
 
 exports.dbConnect = async () => {
-  const uri = await mongoServer.getUri();
+  console.log(`Connecting to MongoMemoryServer`);
+  const mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
   const mongooseOpts = {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -14,6 +16,7 @@ exports.dbConnect = async () => {
 };
 
 exports.dbDisconnect = async () => {
+  console.log(`Disconnecting from MongoMemoryServer`);
   await mongoose.connection.dropDatabase();
   await mongoose.connection.close();
   await mongoServer.stop();
