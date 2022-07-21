d182fb7466ab53690365336c859744bcfc37f0df
CE Martin <16669684+candacemartin@users.noreply.github.com>
2022-07-21 12:37:27 -0400

    supertest updates

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,15 +4,13 @@ const supertest = require('supertest');
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(global.__MONGO_URI__);
 const { dbConnect, dbDisconnect } = require('./utils/dbHandler.utils.js');
-const User = require('../server/models/usersModel');
-const userRoute = require('../server/routes/userRoute');
-const hikeRoute = require('../server/routes/hikeRoute');
-
-beforeAll(async () => dbConnect());
-afterAll(async () => dbDisconnect());
+const User = require('../server/models/User');
+const userRoute = require('../server/routers/userRouter');
+const hikeRoute = require('../server/routers/hikeRouter');
+const { Unstable_Grid2 } = require('@mui/material');
 
 // //helper function for temp server instance:
-function createServer() {
+const createServer = () => {
   const app = express();
   app.use(express.json());
   console.log('inside createServer pre routes');
@@ -20,68 +18,52 @@ function createServer() {
   app.use('/api/hikes', hikeRoute);
   console.log('inside createServer pre return');
   return app;
-}
-console.log(createServer);
-
+};
 const testApp = createServer();
 
-//before testing get all user route, create a temp user record:
-test('GET /api/users', async () => {
-  const user = await User.create({
-    firstName: 'supertestFirst',
-    lastName: 'supertestLast',
-    location: 'superville',
-    email: 'super@supertest.com',
-    password: 'supertest',
-  });
+beforeAll(async () => dbConnect());
+afterAll(async () => dbDisconnect());
 
-  //test get all users route:
-  await supertest(testApp)
-    .get('/api/users/getall')
-    .expect(200)
-    .then((response) => {
-      //check response type and length
-      expect(Array.isArray(response.body)).toBeTruthy();
-      expect(response.body.length).toEqual(1);
-      console.log('in expect statements1');
-      //check response data
-      expect(response.body[0]._id).toBe(user.id);
-      expect(response.body[0].firstName).toBe(user.firstName);
-      expect(response.body[0].lastName).toBe(user.lastName);
-      expect(response.body[0].location).toBe(user.location);
-      expect(response.body[0].email).toBe(user.email);
-      expect(response.body[0].password).toBe(user.password);
-      console.log('in expect statements2');
+describe('testing user routes', () => {
+  //before testing /getall route, create a temp user record:
+  it('gets all users', async () => {
+    const user1 = await User.create({
+      firstName: 'user1First',
+      lastName: ' user1Last',
+      location: 'user1ville',
+      email: 'user1@user1.com',
+      password: 'user1pass',
     });
-});
-
-//before testing login route, create temp user record:
-test('POST /api/users', async () => {
-  const user = await User.create({
-    firstName: 'supertestFirst',
-    lastName: 'supertestLast',
-    location: 'superville',
-    email: 'super@supertest.com',
-    password: 'supertest',
-  });
-  console.log('user', user);
-
-  await supertest(testApp)
-    .post('/api/users/login')
-    .send(user)
-    .expect(200)
-    .then(async (response) => {
-      // Check the response
-      console.log('inside post and response looks like:', response);
-      //expect(response.body._id).toBeTruthy();
-      // expect(response.body).toBe(user);
-      // expect(response.body.firstName).toBe(user.firstName);
-      // Check the data in the database
-      // const loggedUser = await User.findOne({
-      // email: response.body.user.email,
-      // });
-      // console.log('loggedUser', loggedUser);
-      // expect(loggedUser).toBeTruthy();
-      // expect(loggedUser.firstName).toBe(user.firstName);
+    const user2 = await User.create({
+      firstName: 'user2First',
+      lastName: ' user2Last',
+      location: 'user2ville',
+      email: 'user2@user2.com',
+      password: 'user2pass',
     });
+
+    await supertest(testApp)
+      .get('/api/users/getall')
+      .expect(200)
+      .then((response) => {
+        //check response type and length
+        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body.length).toEqual(2);
+        console.log('in expect statements1');
+        //check response data
+        expect(response.body[0]._id).toBe(user1.id);
+        expect(response.body[0].firstName).toBe(user1.firstName);
+        expect(response.body[0].lastName).toBe(user1.lastName);
+        expect(response.body[0].location).toBe(user1.location);
+        expect(response.body[0].email).toBe(user1.email);
+        expect(response.body[0].password).toBe(user1.password);
+        expect(response.body[1]._id).toBe(user2.id);
+        expect(response.body[1].firstName).toBe(user2.firstName);
+        expect(response.body[1].lastName).toBe(user2.lastName);
+        expect(response.body[1].location).toBe(user2.location);
+        expect(response.body[1].email).toBe(user2.email);
+        expect(response.body[1].password).toBe(user2.password);
+        console.log('in expect statements2');
+      });
+  });
 });
